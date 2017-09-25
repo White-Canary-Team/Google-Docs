58d661488284e03132e46ff5c9b7fb4045640b59
Ruston Reformado <rustonreformado@gmail.com>
2017-09-25 16:46:39 -0600

    landing page styling, readme

--- googledoc/src/components/Landing/Landing.jsx
+++ googledoc/src/components/Landing/Landing.jsx
@@ -4,7 +4,15 @@ class Landing extends Component {
     render() {
         return (
             <div>
-                <a href="http://localhost:3001/auth"><button>Log In</button></a>
+                <div className='landing-header'>
+                    <p className='google-logo'>Google</p>
+                    <a className='login-button' href="http://localhost:3001/auth"><button>Log In</button></a>
+                </div>
+                <div className='landing-main'>
+                    <a href="#" className="go-to-google-button">Go to Google Docs</a>
+                </div>
+                
+                
             </div>
         );
     }
