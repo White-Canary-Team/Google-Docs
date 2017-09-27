31b3547fcc0b7d8444f7efcdeb44637cd4a897af
Ruston Reformado <rustonreformado@gmail.com>
2017-09-27 15:30:33 -0600

    working on header for quill document page

--- googledoc/src/Router/Router.jsx
+++ googledoc/src/Router/Router.jsx
@@ -5,6 +5,7 @@ import Quill from '../components/Quill/Quill.jsx'
 import Sheets from '../components/Sheets/Sheets.jsx'
 import Home from '../components/Home/Home.jsx'
 
+import QuillHeader from '../components/Quill/QuillHeader/QuillHeader.jsx';
 
 export default (
     <Switch>
@@ -13,7 +14,7 @@ export default (
         <Route component={Quill} path='/Quill' exact />
         <Route component={Sheets} path='/Sheets' exact />
 
-
+        <Route component={QuillHeader} path='/testquill' />
     </Switch>
 )
 
