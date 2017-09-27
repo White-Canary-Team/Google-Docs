17dacc52d605acf0c94bf260f3f3062db324f3b8
christopher anderson <canderson0289@gmail.com>
2017-09-27 16:51:06 -0600

    fixed it

--- googledoc/src/Router/Router.jsx
+++ googledoc/src/Router/Router.jsx
@@ -4,6 +4,7 @@ import Landing from '../components/Landing/Landing.jsx'
 import Quill from '../components/Quill/Quill.jsx'
 import Sheets from '../components/Sheets/Sheets.jsx'
 import Home from '../components/Home/Home.jsx'
+import Profile from '../components/Profile/Profile.jsx'
 
 import QuillHeader from '../components/Quill/QuillHeader/QuillHeader.jsx';
 
@@ -13,7 +14,7 @@ export default (
         <Route component={Home} path='/Home' exact />
         <Route component={Quill} path='/Quill' exact />
         <Route component={Sheets} path='/Sheets' exact />
-
+        <Route component={Profile} path='/profile' exact />
         <Route component={QuillHeader} path='/testquill' />
     </Switch>
 )
