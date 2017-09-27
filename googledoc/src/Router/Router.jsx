5d6593859b48d31792c3208b9ff5e8d376d267a2
Alex Hill <big_al_hill@comcast.net>
2017-09-27 16:34:53 -0600

    Profile Component

--- googledoc/src/Router/Router.jsx
+++ googledoc/src/Router/Router.jsx
@@ -4,6 +4,8 @@ import Landing from '../components/Landing/Landing.jsx'
 import Quill from '../components/Quill/Quill.jsx'
 import Sheets from '../components/Sheets/Sheets.jsx'
 import Home from '../components/Home/Home.jsx'
+import Profile from '../components/Profile/Profile.jsx'
+
 
 
 export default (
@@ -12,6 +14,8 @@ export default (
         <Route component={Home} path='/Home' exact />
         <Route component={Quill} path='/Quill' exact />
         <Route component={Sheets} path='/Sheets' exact />
+        <Route component={Profile} path='/Profile' exact />
+
 
 
     </Switch>
