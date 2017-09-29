3cbf64bbf93c3f8e060c76240f3f2e203b06f009
Charles Murphy <cwmurphy7@gmail.com>
2017-09-29 12:53:54 -0600

    Add font and font size selectors

--- googledoc/src/components/Sheets/Sheets.jsx
+++ googledoc/src/components/Sheets/Sheets.jsx
@@ -12,7 +12,8 @@ import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import FormatColorText from 'material-ui/svg-icons/editor/format-color-text';
 import FormatColorFill from 'material-ui/svg-icons/editor/format-color-fill';
-
+import Undo from 'material-ui/svg-icons/content/undo';
+import Redo from 'material-ui/svg-icons/content/redo';
 // import fontColorText from 'material-design-icons/icons/editor/alarm';
 
 let socket;
@@ -35,6 +36,8 @@ class Sheets extends Component {
       activeSelection:[null,null,null,null],
       latestColor: 'black',
       latestBg: 'bg-white',
+      latestFont: 'arial',
+      latestFontSize: 'ten-point'
     }
     
     this.handleChange = this.handleChange.bind(this)
@@ -46,6 +49,8 @@ class Sheets extends Component {
     this.getStyles = this.getStyles.bind(this)
     this.handleColorChange = this.handleColorChange.bind(this)
     this.handleBgChange = this.handleBgChange.bind(this)
+    this.handleFontChange = this.handleFontChange.bind(this)
+    this.handleFontSizeChange = this.handleFontSizeChange.bind(this)
   }
 
   //On mount, take fillData info from state and put into a 50x50 matrix, then push this matrix to state as table
@@ -74,7 +79,7 @@ class Sheets extends Component {
     for (let i=0;i<this.state.rows;i++){
       let row = [];
       for (let j=0;j<this.state.columns;j++){
-        row.push({bg:'',color:''})
+        row.push({bg:this.state.latestBg,color:this.state.latestColor,font:this.state.latestFont, fontSize: this.state.latestFontSize})
       }
       tempStyles.push(row);
     }
@@ -208,7 +213,9 @@ class Sheets extends Component {
       for (let i=selected[0];i<=selected[2];i++){
         for (let j=selected[1];j<=selected[3];j++){
           let bg = tempStyles[i][j].bg
-          tempStyles[i].splice(j,1,{bg:bg,color:value})
+          let font = tempStyles[i][j].font
+          let fontSize = tempStyles[i][j].fontSize
+          tempStyles[i].splice(j,1,{bg:bg,font:font,fontSize:fontSize,color:value})
         }
       }
       this.setState({styles: tempStyles, latestColor: value})
@@ -221,20 +228,54 @@ class Sheets extends Component {
       for (let i=selected[0];i<=selected[2];i++){
         for (let j=selected[1];j<=selected[3];j++){
           let color = tempStyles[i][j].color
-          tempStyles[i].splice(j,1,{color:color,bg:value})
+          let font = tempStyles[i][j].font
+          let fontSize = tempStyles[i][j].fontSize
+          tempStyles[i].splice(j,1,{color:color,font:font,fontSize:fontSize,bg:value})
         }
       }
-      // let valColor = value.split('').splice(3).join('');
-      // console.log(valColor)
       this.setState({styles: tempStyles, latestBg:value})
-      console.log(this.state.latestBg)
+      
     }
+    
+  }
+  handleFontChange(event,key,value){
+    let selected = this.state.activeSelection.slice();
+    if (selected[0]){
+      let tempStyles = this.state.styles.slice();
+      for (let i=selected[0];i<=selected[2];i++){
+        for (let j=selected[1];j<=selected[3];j++){
+          let color = tempStyles[i][j].color;
+          let bg = tempStyles[i][j].bg;
+          let fontSize = tempStyles[i][j].fontSize
+          tempStyles[i].splice(j,1,{color:color,bg:bg,fontSize:fontSize,font:value})
+        }
+      }
+      this.setState({styles: tempStyles, latestFont:value})
+    } else this.setState({latestFont: value})
+    
+  }
+  handleFontSizeChange(event,key,value){
+    let selected = this.state.activeSelection.slice();
+    if (selected[0]){
+      let tempStyles = this.state.styles.slice();
+      for (let i=selected[0];i<=selected[2];i++){
+        for (let j=selected[1];j<=selected[3];j++){
+          let color = tempStyles[i][j].color;
+          let bg = tempStyles[i][j].bg;
+          let font = tempStyles[i][j].font
+          tempStyles[i].splice(j,1,{color:color,bg:bg,font:font,fontSize:value})
+        }
+      }
+      this.setState({styles: tempStyles, latestFontSize:value})
+    } else this.setState({latestFontSize: value})
     
   }
   getStyles(row,col){
     let bg = this.state.styles[row][col].bg;
     let color = this.state.styles[row][col].color;
-    return (bg + ' ' + color);
+    let font = this.state.styles[row][col].font;
+    let fontSize = this.state.styles[row][col].fontSize;
+    return (bg + ' ' + color + ' ' + font + ' ' + fontSize);
   }
 
   render() {
@@ -246,22 +287,22 @@ class Sheets extends Component {
 
           <div className='undo-redo'>
             <div className='undo'  onClick={()=>this.handleUndo()}>
-              <i className='fa fa-undo'></i>
+              <Undo style={{color: 'gray', fontSize: '4px'}}/>
             </div>
             <div className='redo'  onClick={()=>this.handleRedo()}>
-              <i className='fa fa-repeat'></i>
+              <Redo style={{color: 'gray', fontSize: '4px'}}/>
             </div>
           </div>
-          <div>
-          <DropDownMenu value={this.state.zoom} onChange={this.handleZoom} className='zoom-select'>
-            <MenuItem value={.50} primaryText="50%" />
-            <MenuItem value={.75} primaryText="75%" />
-            <MenuItem value={.90} primaryText="90%" />
-            <MenuItem value={1.00} primaryText="100%" />
-            <MenuItem value={1.25} primaryText="125%" />
-            <MenuItem value={1.5} primaryText="150%" />
-            <MenuItem value={2.00} primaryText="200%" />
-          </DropDownMenu>
+          <div className='zoom-select-container'>
+            <DropDownMenu value={this.state.zoom} onChange={this.handleZoom} className='zoom-select' selectedMenuItemStyle={{color:'gray'}} iconStyle={{fill: '#3b3b3b', marginTop: '-4px'}}>
+              <MenuItem value={.50} primaryText="50%" />
+              <MenuItem value={.75} primaryText="75%" />
+              <MenuItem value={.90} primaryText="90%" />
+              <MenuItem value={1.00} primaryText="100%" />
+              <MenuItem value={1.25} primaryText="125%" />
+              <MenuItem value={1.5} primaryText="150%" />
+              <MenuItem value={2.00} primaryText="200%" />
+            </DropDownMenu>
           </div>
           
 
@@ -271,35 +312,26 @@ class Sheets extends Component {
             <div className='less'onClick={()=>this.handleDataType('less')}>{'.0<'}</div>
             <div className='more'onClick={()=>this.handleDataType('more')}>{'.0>'}</div> 
           </div>
-         {/* <div className='color-select'>
-            <select onChange={(event)=>this.handleColorChange(event.target.value)} defaultValue={'black'}>
-              <option value={'black'}>black</option>
-              <option value={'red'}>red</option>
-              <option value={'orange'}>orange</option>
-              <option value={'yellow'}>yellow</option>
-              <option value={'green'}>green</option>
-              <option value={'cyan'}>cyan</option>
-              <option value={'cornflowerblue'}>cornflower blue</option>
-              <option value={'blue'}>blue</option>
-              <option value={'purple'}>purple</option>
-              <option value={'magenta'}>magenta</option>
-              <option value={'white'}>white</option>
-            </select>
-          </div> */}
+         
+
+          <div className='font-select-container'>
+            <DropDownMenu value={this.state.latestFont} className='font-select' onChange={this.handleFontChange} selectedMenuItemStyle={{fontFamily:this.state.latestFont, color:'gray'}} iconStyle={{fill: '#3b3b3b', marginTop: '-4px'}}>
+              {/* iconStyle={{fill: '#3b3b3b'}} */}
+              <MenuItem value={'arial'} primaryText="Arial" style={{fontFamily: 'Arial'}}/>  
+              <MenuItem value={'open-sans'} primaryText="Open Sans" style={{fontFamily: 'Open Sans'}}/>
+              <MenuItem value={'roboto'} primaryText="Roboto" style={{fontFamily: 'Roboto'}}/>
+              <MenuItem value={'verdana'} primaryText="Verdana" style={{fontFamily: 'Verdana'}}/>
+            </DropDownMenu>
+          </div>
+          <div className='font-size-select-container'>
+            <DropDownMenu value={this.state.latestFontSize} className='font-size-select' onChange={this.handleFontSizeChange} selectedMenuItemStyle={{color:'gray'}}  iconStyle={{fill: '#3b3b3b', marginTop: '-4px'}}>
+              <MenuItem value={'eight-point'} primaryText="8" />  
+              <MenuItem value={'ten-point'} primaryText="10" />
+              <MenuItem value={'twelve-point'} primaryText="12" />
+            </DropDownMenu>
+          </div>
+
           <div className='color-select'>
-            {/* <DropDownMenu value={this.state.latestColor} onChange={(event, key, value)=>this.handleColorChange(value)}  selectedMenuItemStyle={ {backgroundColor:'#f5f5f5', color:this.state.latestColor}} style={{color:this.state.latestColor,backgroundColor: 'red'}}>
-              <MenuItem value={'black'} primaryText="black" style={ {color: 'black'}} />
-              <MenuItem value={'red'} primaryText="red" style={ {color: 'red'}}/>
-              <MenuItem value={'orange'} primaryText="orange" style={ {color: 'orange'}}/>
-              <MenuItem value={'yellow'} primaryText="yellow" style={ {color: 'yellow'}}/>
-              <MenuItem value={'lime'} primaryText="green" style={ {color: '#51ff3f'}}/>
-              <MenuItem value={'cyan'} primaryText="cyan" style={ {color: 'cyan'}}/>
-              <MenuItem value={'cornflowerblue'} primaryText="cornflowerblue" style={ {color: 'cornflowerblue'}}/>
-              <MenuItem value={'blue'} primaryText="blue" style={ {color: 'blue'}}/>
-              <MenuItem value={'purple'} primaryText="purple" style={ {color: 'purple'}}/>
-              <MenuItem value={'magenta'} primaryText="magenta" style={ {color: 'magenta'}}/>
-              <MenuItem value={'white'} primaryText="white" style={ {color: 'gray'}}/>
-            </DropDownMenu> */}
 
             <IconMenu
               iconButtonElement={<IconButton><FormatColorText /></IconButton>}
