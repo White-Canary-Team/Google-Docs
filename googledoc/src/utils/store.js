e7e9cfb6ef0d99b0260b5a0e1e1253804eb560ad
christopher anderson <canderson0289@gmail.com>
2017-09-29 15:12:45 -0600

    Addded promise midleware, and documents to Redux"

--- googledoc/src/utils/store.js
+++ googledoc/src/utils/store.js
@@ -1,8 +1,11 @@
-import {createStore } from 'redux'
+import {createStore, applyMiddleware } from 'redux'
 import reducer from './../ducks/reducer.js'
+import reduxPromiseMiddleware from 'redux-promise-middleware'
 
 
 
-let store = createStore(reducer)
+
+let store = createStore(reducer,applyMiddleware(reduxPromiseMiddleware()))
+
 
 export default store
