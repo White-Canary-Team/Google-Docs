5408754e627e0f680c78b8d160f4d1fe2dd5dc36
christopher anderson <canderson0289@gmail.com>
2017-10-02 10:46:02 -0600

    Added tabs for word only or excel only docs

--- googledoc/src/components/Home/Home.jsx
+++ googledoc/src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ import Header from './../Header/Header.jsx'
 import { Link } from 'react-router-dom'
 import Paper from 'material-ui/Paper'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Tabs, Tab } from 'material-ui/Tabs';
 
 
 
@@ -20,8 +21,8 @@ class Home extends Component {
             searchTerm: '',
             pic: ''
         }
-    this.handleQuill = this.handleQuill.bind(this)
-    this.handleSheet = this.handleSheet.bind(this)
+        this.handleQuill = this.handleQuill.bind(this)
+        this.handleSheet = this.handleSheet.bind(this)
     }
     componentWillMount() {
 
@@ -32,21 +33,21 @@ class Home extends Component {
 
             })
             this.props.emailAdd(this.state.emails, this.state.pic)
-            
+
             axios.post('/user', {
                 email: this.state.emails,
             })
         })
-        axios.get('/allUsers').then(response =>{
-            if(this.state.emails){
-                let uid = response.data.filter((e)=>{
-                    if(e.email === this.state.emails){
-                      
+        axios.get('/allUsers').then(response => {
+            if (this.state.emails) {
+                let uid = response.data.filter((e) => {
+                    if (e.email === this.state.emails) {
+
                         return e.id
                     }
                     return null
                 })
-                
+
                 this.setState({
                     userId: uid[0].id
                 })
@@ -54,61 +55,61 @@ class Home extends Component {
             }
 
 
-    })
+        })
         //axios.get('/documents').then(response =>{
         //    console.log(response.data)
         //    this.setState({
         //        documents: response.data
         //    })
         //})
-}
-componentDidMount(){
-    this.props.getDocs()
-    
-}
-handleQuill(){
-    axios.post('/quill', {
-        title: "untitled document",
-        creator: this.props.userId,
-        doctype: "word"
-    })
-}
+    }
+    componentDidMount() {
+        this.props.getDocs()
 
-handleSheet(){
-    axios.post('/jsheets', {
-        title: "untitled document",
-        creator: this.props.userId,
-        doctype: "excel"
-    })
-}
+    }
+    handleQuill() {
+        axios.post('/quill', {
+            title: "untitled document",
+            creator: this.props.userId,
+            doctype: "word"
+        })
+    }
+
+    handleSheet() {
+        axios.post('/jsheets', {
+            title: "untitled document",
+            creator: this.props.userId,
+            doctype: "excel"
+        })
+    }
 
 
 
 
 
     render() {
-        let filteredDoc = this.props.documents ? this.props.documents.filter((e) =>{
-            if(e.creator === this.props.userId){
+        let filteredDoc = this.props.documents ? this.props.documents.filter((e) => {
+            if (e.creator === this.props.userId) {
                 return e
             }
             return null
-                           
-        }): null
-        console.log(filteredDoc) 
-        let myDocs = filteredDoc ? filteredDoc.map((c,i)=>{
+
+        }) : null
+        console.log(filteredDoc)
+        let myDocs = filteredDoc ? filteredDoc.map((c, i) => {
             console.log("C", c)
             let quillLink = `/quill/${c.id}`;
             let sheetLink = `/sheets/${c.id}`;
-            let docStyle = c.doctype === 'word' ? "2px solid #90CAF9": "2px solid #A5D6A7"
-            let docLink = c.doctype === 'word' ? 
-            <Link to={quillLink}> <Paper className="doc-box" style={{border: docStyle}} zDepth={2}  key={i} rounded={false}> {c.title} {c.id}</Paper></Link> :
-            <Link to={sheetLink}> <Paper className="doc-box" style={{border: docStyle}} zDepth={2} key={i}  rounded={false}>{c.title} {c.id}</Paper></Link>
-            
-            return( docLink)
-            
-            
-        }): null
-        let loader =  this.props.loading ? <div className="loader"> <i className="fa fa-spinner fa-pulse fa-5x fa-fw"></i></div>:myDocs
+            let docStyle = c.doctype === 'word' ? "2px solid #90CAF9" : "2px solid #A5D6A7"
+            let docLink = c.doctype === 'word' ?
+                <Link to={quillLink}> <Paper className="doc-box" style={{ border: docStyle }} zDepth={2} key={i} rounded={false}> {c.title} {c.id}</Paper></Link> :
+                <Link to={sheetLink}> <Paper className="doc-box" style={{ border: docStyle }} zDepth={2} key={i} rounded={false}>{c.title} {c.id}</Paper></Link>
+
+            return (docLink)
+
+
+        }) : null
+        let loader = this.props.loading ? <div className="loader"> <i className="fa fa-spinner fa-pulse fa-5x fa-fw"></i></div> : myDocs
 
         console.log(this.props.loading)
         let style = {
@@ -119,6 +120,44 @@ handleSheet(){
             display: 'inline-block',
             padding: 20
         };
+
+
+
+
+        let filteredDocByType = this.props.documents ? this.props.documents.filter((e) => {
+            if (e.creator === this.props.userId && e.doctype === "word") {
+                return e
+            }
+            return null
+        }) : null
+
+        let wordDocs = filteredDocByType ? filteredDocByType.map((c, i) => {
+            let quillLink = `/quill/${c.id}`;
+            let docStyle = c.doctype === 'word' ? "2px solid #90CAF9" : "2px solid #A5D6A7"
+            let wordLink = c.doctype === 'word' ?
+                <Link to={quillLink}> <Paper className="doc-box" style={{ border: docStyle, marginBottom: '30px' }} zDepth={2} key={i} rounded={false}> {c.title} {c.id}</Paper></Link> :
+                <h1> No word Documents</h1>
+            return (wordLink)
+        }) : <h1> No word Documents</h1>
+
+
+        let filteredDocByTypeExcel = this.props.documents ? this.props.documents.filter((e) => {
+            if (e.creator === this.props.userId && e.doctype === "excel") {
+                return e
+            }
+            return null
+        }) : null
+
+        let excelDocs = filteredDocByTypeExcel ? filteredDocByTypeExcel.map((c, i) => {
+            let sheetLink = `/sheets/${c.id}`;
+            let docStyle = c.doctype === 'excel' ? "2px solid #A5D6A7": null
+            let excelLink = c.doctype === 'excel' ?
+                <Link to={sheetLink}> <Paper className="doc-box" style={{ border: docStyle, marginBottom: '30px' }} zDepth={2} key={i} rounded={false}> {c.title} {c.id}</Paper></Link> :null
+            return (excelLink)
+        }) : <h1> No excel Documents</h1>
+
+
+
         return (
             <MuiThemeProvider>
                 <div>
@@ -132,16 +171,44 @@ handleSheet(){
                         </div>
 
                     </div>
-                    <div className="recent-doc">
-                        <p>Recent Documents</p>
+                    <Tabs>
+                        <Tab label="All Documents">
+                            <div className="recent-doc">
+                                <p>Recent Documents</p>
 
-                        <div className='my-docs'>
-                            
-                            {loader}
-                        </div>
+                                <div className='my-docs'>
+
+                                    {loader}
+                                </div>
 
 
-                    </div>
+                            </div>
+                        </Tab>
+                        <Tab label="Word Documents">
+                            <div className="recent-doc">
+                                <p> Word Documents</p>
+
+                                <div className="my-docs">
+                                    {wordDocs}
+                                </div>
+
+                            </div>
+
+                        </Tab>
+
+                        <Tab label="ExcelDocs">
+                            <div className="recent-doc">
+                                <p> Excel Documents</p>
+
+                                <div className="my-docs">
+                                    {excelDocs}
+                                    </div>
+
+                            </div>
+
+                        </Tab>
+
+                    </Tabs>
 
                 </div>
             </MuiThemeProvider>
